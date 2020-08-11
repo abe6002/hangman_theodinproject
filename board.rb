587d40b6fe312d8ae3f36cfb16f3ed9ebe7b4ade
abe6002 <dawsonabraham16@gmail.com>
2020-08-10 19:50:42 -0600

    Added initialize and  guessing and checking functionality to classes

--- board.rb
+++ board.rb
@@ -1,1 +1,32 @@
 #Board class for Hangman Project
+#should have method for loading dictionary and picking word - should initialize with word
+#should have method to build board with empty spaces - during initialize
+#should have method to guess (player class)
+#should have method to check for win
+#should have method for computer guess (computer class?)
+#should have method to swap turns
+
+require_relative "player"
+
+class Board
+
+    def initialize(player_name)
+        @word = File.readlines("5desk.txt").sample.chomp
+        @player = Player.new(player_name)
+        @board = ""
+
+        @word.each_char {|char| @board += "_"}
+    end
+
+    def check_for_letter
+        letter = @player.guess?
+
+        if @word.include?(letter)
+            idx = @word.index(letter)
+            @board[idx] = letter
+        end
+
+
+    end
+
+end
--- player.rb
+++ player.rb
@@ -1,1 +1,30 @@
 #Player class for Hangman Project
+
+class Player
+
+    attr_accessor :guesses
+
+    def initialize(name)
+        @name = name
+        @turn = true
+        @guesses = []
+    end
+
+    def valid_guess?(guess) #need to return something indicating false
+        alpha = ("a".."z").to_a
+        !@guesses.include?(guess) && alpha.include?(guess)
+    end
+
+    def guess? #need to return something indicating false
+        puts "#{@name}, enter your guess:"
+        guess = gets.to_s.downcase.chomp
+        if valid_guess?(guess)
+            @guesses << guess
+        end
+
+        self.guesses.last
+    end
+
+    
+
+end
